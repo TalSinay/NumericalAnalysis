a5c3be48334dc231ab97ab0f8d298a54eaf8c7ed
Talsinay <talsinay1000@gamil.com>
2022-03-07 16:21:12 +0200

    machine_epsilon

--- first_upload.py
+++ first_upload.py
@@ -1,3 +1,10 @@
-print('tal,lin,yuval')
-print('lin')
-print('yuval')
+def machinePrecision(f=float):
+    machine=f(1)
+    while f(1)+f(machine)!=f(1):
+        new_machine=machine
+        machine=f(machine)/f(2)
+    return new_machine
+
+
+print("before:",abs(3.0*(4.0/3.0-1)-1))
+print("after:",abs(3.0*(4.0/3.0-1)-1)-machinePrecision())
