59b0f09e9f92cec79b568d8b9dd88d836e152896
Talsinay <talsinay1000@gamil.com>
2022-05-10 13:20:29 +0300

    fix

--- crossingMethod.py
+++ crossingMethod.py
@@ -72,42 +72,51 @@ def secant_method(f,start_point,end_point,e):
 
 def main():
     x = var('x')  # the possible variable names must be known beforehand...
+    x = symbols('x')
     user_input = input("Enter function\n")
     f= sympify(user_input)
     start_point=float(input("Enter start point\n"))
     b=float(input("Enter end point\n"))
     e=0.0001
-    x = symbols('x')
     fd = nigzeret(f, x)
-    a=(abs(start_point)+abs(b))*10
+    a=int((abs(start_point)+abs(b))*10)
     choice=int(input("which methon you want to use:\n1->Bisection Method\n2->Newton Raphson\n3->secant method"))
-    match choice:
-        case 1:
-            for i in range(a):
-                x1 = start_point + i * 0.1
-                x2 = x1 + 0.1
-                if(f(x1)*f(x2)<0):
-                    print("for f(X)->",Bisection_Method(f,x1,x2,e))
-                if fd(x1)*fd(x2)<0:
-                    print("for f'(X)->",Bisection_Method(fd, x1, x2, e))
-        case 2:
-            for i in range(a):
-                x1 = start_point + i * 0.1
-                x2 = x1 + 0.1
-                if (f(x1) * f(x2) < 0):
-                        print("for f(X)->", Newton_Raphson(f, x1, x2, e))
-                if fd(x1) * fd(x2) < 0:
-                    print("for f'(X)->", Newton_Raphson(fd, x1, x2, e))
+
+    if choice == 1:
+        for i in range(a):
+            x1 = start_point + i * 0.1
+            x2 = x1 + 0.1
+            if(f(x1)*f(x2))<0:
+                print("for f(X)->",Bisection_Method(f,x1,x2,e))
+            if fd(x1)*fd(x2)<0:
+                print("a critical point!")
+                o=Bisection_Method(fd, x1, x2, e)
+                if f(o)==0:
+                    print("negative root for f'(X),positive for f(x)->",Bisection_Method(fd, x1, x2, e))
+    if choice== 2:
+        for i in range(a):
+            x1 = start_point + i * 0.1
+            x2 = x1 + 0.1
+            if (f(x1) * f(x2)) < 0:
+                    print("for f(X)->", Newton_Raphson(f, x1, x2, e))
+            if fd(x1) * fd(x2) < 0:
+                print("a critical point!")
+                o=Newton_Raphson(fd, x1, x2, e)
+                if f(o)==0:
+                    print("negative root for f'(X),positive for f(x)->", Newton_Raphson(fd, x1, x2, e))
 
 
-        case 3:
-            for i in range(a):
-                x1 = start_point + i * 0.1
-                x2 = x1 + 0.1
-                if (f(x1) * f(x2) < 0):
-                    print("for f(X)->", secant_method(f, x1, x2, e))
-                if fd(x1) * fd(x2) < 0:
-                    print("for f'(X)->", secant_method(fd, x1, x2, e))
+    if choice== 3:
+        for i in range(a):
+            x1 = start_point + i * 0.1
+            x2 = x1 + 0.1
+            if (f(x1) * f(x2)) < 0:
+                print("for f(X)->", secant_method(f, x1, x2, e))
+            if fd(x1) * fd(x2) < 0:
+                print("a critical point!")
+                o=secant_method(fd, x1, x2, e)
+                if f(o)==0:
+                    print("negative root for f'(X),positive for f(x)->", secant_method(fd, x1, x2, e))
 
 
 
@@ -117,3 +126,4 @@ def main():
 #print(secant_method(f,start_point,b,e))
 #print(Newton_Raphson(f,start_point,b,e))
 #print(Bisection_Method(f,a,b))
+main()
